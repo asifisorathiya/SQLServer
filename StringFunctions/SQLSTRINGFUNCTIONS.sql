af3f299cc8e998181509f16e0eebcbca0b40cba7
Asif Sorathiya <asifiqbalsorathiya@gamilcom>
2025-09-23 13:15:03 +0300

    SQL STRING FUNCTIONS

---
+++ StringFunctions/SQLSTRINGFUNCTIONS.sql
@@ -0,0 +1,36 @@
+--CONCAT
+
+SELECT CustomerKey, CONCAT(FirstName,' ',LastName) AS [CUSTOMER_NAME]
+FROM DimCustomer
+
+--UPPER
+
+SELECT CustomerKey, UPPER(CONCAT(FirstName,' ',LastName)) AS [CUSTOMER_NAME]
+FROM DimCustomer
+
+
+--LOWER
+
+SELECT CustomerKey, LOWER(CONCAT(FirstName,' ',LastName)) AS [CUSTOMER_NAME]
+FROM DimCustomer
+
+
+--TRIM
+SELECT TRIM('                  JOHN   ') AS [TRIMMED_DATA]
+
+
+--REPLACE
+SELECT FirstName,REPLACE(PHONE,'-','/') AS PHONE
+FROM DimCustomer
+
+--LEN
+
+SELECT FirstName, LEN(FirstName) AS First_Name
+FROM DimCustomer
+
+--LEFT,RIGHT
+SELECT FirstName, LEFT(FirstName,2) AS LEFT_LEN,RIGHT(FirstName,2) AS RIGHT_LEN
+FROM DimCustomer
+
+
+
